11569e30ca91977e49b8f4a24af1047eacac1ca7
Tho Nguyen Viet <69102801+thonv0302@users.noreply.github.com>
2023-10-11 07:28:07 +0700

    update drag drop

--- src/components/global/admin/product/ProductVariant.vue
+++ src/components/global/admin/product/ProductVariant.vue
@@ -1,141 +1,168 @@
 <template>
-  <draggable
-    item-key="id"
-    handle=".drag-handle"
-    :animation="100"
-    v-model="options"
-    group="options"
-    ghostClass="sortable-ghost"
-    :force-fallback="true"
-    @start="onChange"
-    @end="onEnd"
-  >
-    <template #item="{ element: option, index: optionIdx }">
-      <div class="rounded bg-slate-50 mb-2" @mouseup="dragging(option, false)">
+  <VeeForm v-slot="{ errors }">
+    <draggable
+      item-key="id"
+      handle=".drag-handle"
+      :animation="100"
+      v-model="options"
+      group="options"
+      ghostClass="sortable-ghost"
+      :force-fallback="true"
+      @start="onChange"
+      @end="onEnd"
+    >
+      <template #item="{ element: option, index: optionIdx }">
         <div
-          class="option py-1 px-2"
-          v-if="!option.isDraging && !option.isDone"
+          class="rounded bg-slate-50 mb-2"
+          @mouseup="dragging(option, false)"
         >
-          <span class="text-sm ms-10"> Option name</span>
-          <div class="flex">
-            <div
-              class="drag-handle flex cursor-move w-8 h-8 justify-center items-center me-2"
-              @mousedown="dragging(option, true)"
-            >
-              <span>⠋</span>
+          <div
+            class="option py-1 px-2"
+            v-if="!option.isDraging && !option.isDone"
+          >
+            <span class="text-sm ms-10"> Option name</span>
+            <div class="flex">
+              <div
+                class="drag-handle flex cursor-move w-8 h-8 justify-center items-center me-2"
+                @mousedown="dragging(option, true)"
+              >
+                <span>⠋</span>
+              </div>
+              <div class="flex-1">
+                <VeeField
+                  :name="`option${option.id}`"
+                  :id="`option${option.id}`"
+                  type="text"
+                  rules="required"
+                  v-model="option.title"
+                  :class="[
+                    'block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none text-sm',
+                    {
+                      'ring-red-500 border-red-500':
+                        errors[`option${option.id}`],
+                    },
+                  ]"
+                />
+                <ErrorMessage
+                  :name="`option${option.id}`"
+                  class="mt-2 text-sm text-red-600"
+                />
+              </div>
+              <a
+                role="button"
+                class="p-2 inline-block"
+                @click="removeOption(optionIdx)"
+              >
+                <TrashIcon class="w-4 h-4 text-slate-500" />
+              </a>
             </div>
-            <VeeField
-              :name="`option` + option.id"
-              :id="`option` + option.id"
+          </div>
+          <draggable
+            v-model="option.values"
+            handle=".drag-handle"
+            :group="{ name: 'values', pull: false }"
+            :animation="150"
+            item-key="id"
+            :force-fallback="true"
+          >
+            <template #header>
+              <div
+                v-if="!option.isDraging && !option.isDone"
+                class="text-sm ms-14 mt-2"
+              >
+                Option values
+              </div>
+            </template>
+            <template #item="{ element: value, index: valueIdx }">
+              <div class="rounded bg-slate-50">
+                <div
+                  class="option flex py-1 ps-1 pe-2"
+                  v-if="!option.isDraging && !option.isDone"
+                >
+                  <div
+                    class="drag-handle cursor-move w-8 h-8 justify-center items-center ms-6"
+                  >
+                    <span>⠋</span>
+                  </div>
+                  <div class="flex-1">
+                    <VeeField
+                      :name="`option${option.id}value${value.id}`"
+                      :id="`option${option.id}value${value.id}`"
+                      type="text"
+                      rules="required"
+                      v-model="value.title"
+                      :class="[
+                        'block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none text-sm',
+                        {
+                          'ring-red-500 border-red-500':
+                            errors[`option${option.id}value${value.id}`],
+                        },
+                      ]"
+                    />
+                    <ErrorMessage
+                      :name="`value${value.id}`"
+                      class="mt-2 text-sm text-red-600"
+                    />
+                  </div>
+                  <a
+                    role="button"
+                    class="p-2 inline-block"
+                    @click="removeValue(optionIdx, valueIdx)"
+                  >
+                    <TrashIcon class="w-4 h-4 text-slate-500" />
+                  </a>
+                </div></div
+            ></template>
+          </draggable>
+          <div
+            v-if="!option.isDraging && !option.isDone"
+            class="py-1 ps-14 pe-10"
+          >
+            <input
               type="text"
-              rules="required"
-              v-model="option.title"
+              @keyup.enter="addValue($event, option)"
               :class="[
                 'block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none text-sm',
               ]"
             />
             <a
               role="button"
-              class="p-2 inline-block"
-              @click="removeOption(optionIdx)"
+              class="my-2 inline-block shadow text-sm py-1 px-2 border transition-all hover:bg-gray-100 rounded-md me-2"
+              @click="option.isDone = true"
+              type="submit"
             >
-              <TrashIcon class="w-4 h-4 text-slate-500" />
+              Done
             </a>
           </div>
-        </div>
-        <draggable
-          v-model="option.values"
-          handle=".drag-handle"
-          :group="{ name: 'values', pull: false }"
-          :animation="150"
-          item-key="id"
-          :force-fallback="true"
-        >
-          <template #header>
-            <div
-              v-if="!option.isDraging && !option.isDone"
-              class="text-sm ms-14 mt-2"
-            >
-              Option values
-            </div>
-          </template>
-          <template #item="{ element: value, index: valueIdx }">
-            <div class="rounded bg-slate-50">
-              <div
-                class="option flex py-1 ps-1 pe-2"
-                v-if="!option.isDraging && !option.isDone"
-              >
-                <div
-                  class="drag-handle cursor-move w-8 h-8 justify-center items-center ms-6"
-                >
-                  <span>⠋</span>
+          <div v-if="option.isDraging || option.isDone">
+            <div class="option flex py-3 px-5">
+              <span class="me-5 drag-handle cursor-move">⠋</span>
+              <div class="text-sm flex-1">
+                <div class="flex justify-between">
+                  <h6 class="font-medium">{{ option.title }}</h6>
+                  <a
+                    @click="option.isDone = false"
+                    class="shadow-sm text-sm py-1 px-2 border transition-all hover:bg-gray-100 rounded-md"
+                  >
+                    Edit
+                  </a>
                 </div>
-                <VeeField
-                  :name="`value` + value.id"
-                  :id="`value` + value.id"
-                  type="text"
-                  rules="required"
-                  v-model="value.title"
-                  :class="[
-                    'block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none text-sm',
-                  ]"
-                />
-                <a
-                  role="button"
-                  class="p-2 inline-block"
-                  @click="removeValue(optionIdx, valueIdx)"
-                >
-                  <TrashIcon class="w-4 h-4 text-slate-500" />
-                </a>
-              </div></div
-          ></template>
-        </draggable>
-        <div
-          v-if="!option.isDraging && !option.isDone"
-          class="py-1 pe-2 ps-14 pe-10"
-        >
-          <input
-            type="text"
-            @keyup.enter.stop="addValue($event, option)"
-            :class="[
-              'block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none text-sm',
-            ]"
-          />
-          <a
-            role="button"
-            class="my-2 inline-block shadow text-sm py-1 px-2 border transition-all hover:bg-gray-100 rounded-md me-2"
-            @click="option.isDone = true"
-          >
-            Done
-          </a>
-        </div>
-        <div v-if="option.isDraging || option.isDone">
-          <div class="option flex py-3 px-5">
-            <span class="me-5 drag-handle cursor-move">⠋</span>
-            <div class="text-sm flex-1">
-              <div class="flex justify-between">
-                <h6 class="font-medium">{{ option.title }}</h6>
-                <button
-                  @click="option.isDone = false"
-                  class="shadow-sm text-sm py-1 px-2 border transition-all hover:bg-gray-100 rounded-md"
-                >
-                  Edit
-                </button>
-              </div>
-              <div class="mt-2">
-                <span
-                  v-for="value in option.values"
-                  class="rounded-lg px-3 py-2 bg-slate-200 me-2"
-                >
-                  {{ value.title }}</span
-                >
+                <div class="mt-2">
+                  <span
+                    v-for="value in option.values"
+                    class="rounded-lg px-3 py-2 bg-slate-200 me-2"
+                  >
+                    {{ value.title }}</span
+                  >
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </template>
-  </draggable>
+      </template>
+    </draggable>
+  </VeeForm>
+  <pre>{{ options }}</pre>
   <div>
     <hr class="-mx-3" />
     <a
@@ -162,6 +189,7 @@ const removeValue = (optionIdx: number, valueIdx: number) => {
 };
 
 const addValue = (e: any, option: any) => {
+  e.stopPropagation();
   option.values.push({
     title: e.target.value,
     id: nanoid(),
